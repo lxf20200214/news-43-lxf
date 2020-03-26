8c077ea751539f1c3492f0913b9f4e51b30e0308
lxf <1035561057@qq.com>
2020-03-26 22:45:30 +0800

    使用vant表单组件登录成功

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,22 +8,31 @@
     <div class="logo">
       <span class="iconfont iconnew"></span>
     </div>
-    <!-- 表单 -->
-    <div class="form">
-      <div class="form-item">
-        <input placeholder="手机号码" v-model="form.username" />
+    <!-- 使用vant的表单 -->
+    <!-- vant-form是表单的组件,@submit是表单按钮提交的事件 -->
+    <van-form @submit="onSubmit" class="form">
+      <!-- van-field是表单的字段 -->
+      <!-- rules是表单子弹的规则,required表示这个输入框是必填 -->
+      <van-field
+        v-model="form.username"
+        name="手机号码"
+        placeholder="手机号码"
+        :rules="[{ required: true, message: '请填写手机号码' }]"
+      />
+      <van-field
+        v-model="form.password"
+        type="password"
+        name="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div>
+        <!--如果这个按钮是在van-form组件内部,并且按钮的native-type='submit',说明这个按钮就会触发submit事件 -->
+        <van-button round block type="info" native-type="submit">
+          提交
+        </van-button>
       </div>
-      <div class="form-item">
-        <input
-          type="password"
-          placeholder="请输入密码"
-          v-model="form.password"
-        />
-      </div>
-      <div class="form-item">
-        <button @click="handleClick">登录</button>
-      </div>
-    </div>
+    </van-form>
   </div>
 </template>
 
@@ -39,10 +48,10 @@ export default {
     };
   },
   methods: {
-    // 点击登录按钮的事件
-    handleClick() {
-      // this.$toast("登录成功");
-      // 调用axios发起异步请求,类似$.ajax(类似,不代表一样)
+    // 提交表单时候触发的事件,该事件通过校验才会触发
+    // values是表单的返回的值，这里的values我们用不上，数据可以在this.form里面拿
+    onSubmit(values) {
+      // 调用axios发起异步请求, 类似$.ajax(类似, 不代表一样);
       this.$axios({
         // 接口地址
         url: "http://127.0.0.1:3000/login",
@@ -53,7 +62,7 @@ export default {
         data: this.form
         // .then方法里面的函数就是成功的回调函数
       }).then(res => {
-        // console.log(res);
+        // 获取到返回的信息
         const { message } = res.data;
         // 使用vant的弹窗提示用,success表示成功的弹窗
         this.$toast.success(message);
@@ -80,29 +89,21 @@ export default {
   font-size: 126/360 * 100vw;
   color: #cc3300;
 }
-.form-item {
-  margin-top: 20/360 * 100vw;
-}
-.form-item input {
-  width: 100%;
-  height: 44/360 * 100vw;
-  box-sizing: border-box;
-  line-height: 44/360 * 100vw;
-  background: none;
-  border: none;
-  border-bottom: 1px #999 solid;
-  outline: none;
-  font-size: 16px;
-}
-.form-item button {
-  margin-top: 50/360 * 100vw;
-  width: 100%;
-  height: 48/360 * 100vw;
-  background: #cc3300;
-  color: #fff;
-  border-radius: 50px;
-  border: none;
-  outline: none;
-  font-size: 16px;
+
+.form {
+  .van-cell {
+    padding: 10px 0;
+    font-size: 16px;
+    margin-bottom: 20/360 * 100vw;
+  }
+  .van-cell:not(:last-child)::after {
+    border-bottom: 1px solid #333;
+    left: 0;
+  }
+  .van-button--info {
+    margin-top: 50/360 * 100vw;
+    background-color: #cc3300;
+    border: 1px solid #cc3300;
+  }
 }
 </style>
