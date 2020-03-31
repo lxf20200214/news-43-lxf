a1209e735bcdb2e44d65309fb35f158ed50a7263
lxf <1035561057@qq.com>
2020-03-31 20:54:26 +0800

    通过meta给路由守卫添加访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -18,27 +18,49 @@ const routes = [
   {
     // 个人中心
     path: "/personal",
-    component: () => import("@/views/Personal")
+    component: () => import("@/views/Personal"),
+    // 路由可以通过meta自定义数据,可以选择在路由上随意加上自己的数据
+    // 它的值可以是任意类型
+    meta: {
+      // 代表这个页面需要授权
+      authorization: true
+    }
   },
   {
     // 编辑页
     path: "/edit-profile",
-    component: () => import("@/views/EditProfile")
+    component: () => import("@/views/EditProfile"),
+    meta: {
+      // 代表这个页面需要授权
+      authorization: true
+    }
   },
   {
     // 我的关注页
     path: "/follow",
-    component: () => import("@/views/Follow")
+    component: () => import("@/views/Follow"),
+    meta: {
+      // 代表这个页面需要授权
+      authorization: true
+    }
   },
   {
     // 我的跟帖页
     path: "/comment",
-    component: () => import("@/views/Comment")
+    component: () => import("@/views/Comment"),
+    meta: {
+      // 代表这个页面需要授权
+      authorization: true
+    }
   },
   {
     // 我的收藏页
     path: "/star",
-    component: () => import("@/views/Star")
+    component: () => import("@/views/Star"),
+    meta: {
+      // 代表这个页面需要授权
+      authorization: true
+    }
   }
 ];
 
